7d601c9f0e2f038546cecf823bf6edec8f621661
Pranto Ghosh <prantoas8002@gmail.com>
2026-08-01 22:52:27 +0600

    add home route

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-        res.writeHead(200, { "Content-type": "text/plain" });
-        res.end("server created at 5000 successfully");
+
+    let url = req.url;
+
+    if (url === '/') {
+        fs.readFile("home.html", (err, data) => {
+            if (err) {
+                res.writeHead(404, { "Content-type": "text/html" });
+                res.end("<h1>An error occured</h1>")
+            }
+            else {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+            }
+        })
+    }
+    // res.writeHead(200, { "Content-type": "text/plain" });
+    // res.end("server created at 5000 successfully");
 
 });
 
