584c4abd546929db497b2a5b60d5f1b8582c4285
Pranto Ghosh <prantoas8002@gmail.com>
2026-08-01 23:06:51 +0600

    complete assaignment

--- server.js
+++ server.js
@@ -41,9 +41,10 @@ const server = http.createServer((req, res) => {
             }
         })
     }
-    
-
-
+    else {
+        res.writeHead(404, { "Content-type": "text/html" });
+        res.end("<h1>404 NOT Found</h1>");
+    }
 });
 
 server.listen(5000, () => {
